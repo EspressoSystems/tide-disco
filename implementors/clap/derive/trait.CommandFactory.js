1ec1184e83ccfccb5e52ae2f92b68425ab63fc55
nyospe <nyospe@users.noreply.github.com>
2022-10-06 20:31:02 +0000

    deploy: 966c07f862b8f1ff656da5272d2ce668eecbe03c

--- implementors/clap/derive/trait.CommandFactory.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["tide_disco"] = [{"text":"impl CommandFactory for <a class=\"struct\" href=\"tide_disco/struct.DiscoArgs.html\" title=\"struct tide_disco::DiscoArgs\">DiscoArgs</a>","synthetic":false,"types":["tide_disco::DiscoArgs"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["tide_disco"] = {"name":"","files":["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","request.rs","route.rs","socket.rs"]};
+var sourcesIndex = JSON.parse('{\
+"tide_disco":["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","request.rs","route.rs","socket.rs"]]\
+}');
 createSourceSidebar();
