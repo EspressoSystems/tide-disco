4714c0c8efc842a240499e3d57d085b8ba7d6595
jbearer <jbearer@users.noreply.github.com>
2023-12-18 15:53:44 +0000

    deploy: 7ff4abaebe32d86603fc7978722a8f244c7d73c9

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = JSON.parse('{\
-"tide_disco":["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","request.rs","route.rs","socket.rs"]]\
+"tide_disco":["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","metrics.rs","request.rs","route.rs","socket.rs"]]\
 }');
 createSrcSidebar();
