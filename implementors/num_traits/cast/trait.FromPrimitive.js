d6ff8744c112ebb35e5e4337a9cdb51bca526307
jbearer <jbearer@users.noreply.github.com>
2023-12-21 15:37:54 +0000

    deploy: 4ced31cef8aae769994098d245e026c127801b74

---
+++ implementors/num_traits/cast/trait.FromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"tide_disco":[["impl <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/cast/trait.FromPrimitive.html\" title=\"trait num_traits::cast::FromPrimitive\">FromPrimitive</a> for <a class=\"enum\" href=\"tide_disco/status/enum.StatusCode.html\" title=\"enum tide_disco::status::StatusCode\">StatusCode</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = JSON.parse('{\
-"tide_disco":["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","metrics.rs","request.rs","route.rs","socket.rs"]]\
+"tide_disco":["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","metrics.rs","request.rs","route.rs","socket.rs","status.rs"]]\
 }');
 createSrcSidebar();
