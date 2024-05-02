b10eb1ec5dee5245d3327e012deb70133b8aa781
jbearer <jbearer@users.noreply.github.com>
2024-05-02 19:07:46 +0000

    deploy: a38837e13674d5ffe308e879642e541fa0ae25a2

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["tide_disco",["",[],["api.rs","app.rs","error.rs","healthcheck.rs","lib.rs","method.rs","metrics.rs","middleware.rs","request.rs","route.rs","socket.rs","status.rs","testing.rs"]]]\
+["tide_disco",["",[],["api.rs","app.rs","dispatch.rs","error.rs","healthcheck.rs","lib.rs","method.rs","metrics.rs","middleware.rs","request.rs","route.rs","socket.rs","status.rs","testing.rs"]]]\
 ]'));
 createSrcSidebar();
