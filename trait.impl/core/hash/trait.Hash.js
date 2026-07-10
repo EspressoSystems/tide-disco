6fcc38520cf8a3943ef586bea73abbce321972a2
jbearer <jbearer@users.noreply.github.com>
2026-07-10 22:29:32 +0000

    deploy: a2ee1128699ea44258306d3adeb42a90788f6476

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["disco_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"disco_types/status/struct.StatusCode.html\" title=\"struct disco_types::status::StatusCode\">StatusCode</a>",0]]]]);
+    const implementors = Object.fromEntries([["disco_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"disco_types/status/struct.StatusCode.html\" title=\"struct disco_types::status::StatusCode\">StatusCode</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
